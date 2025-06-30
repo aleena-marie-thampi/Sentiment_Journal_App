5ff64d167d60741aa2b2d69f521f00f439764fbf
aleena-marie-thampi <aleenamariethampi@gmail.com>
2025-06-30 22:35:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -305,5 +305,4 @@ def delete_entry_page(date):
     return redirect(url_for('calendar'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True)
